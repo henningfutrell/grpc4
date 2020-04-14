e12371259bb65223ae0166d1e6297c8b122a1303
henningfutrell <henning.futrell@protonmail.com>
2020-04-14 16:09:24 -0500

    Update install script

--- install_cmake.sh
+++ install_cmake.sh
@@ -1,4 +1,4 @@
 if ! [ -x "$(command -v cmake)" ]; then
-	echo 'Not installed' >&2
-	echo "csce438" | sudo -S apt-get install cmake
+	echo 'Cmake not installed' >&2
+	echo "csce438" | sudo -S apt-get install -y cmake
 fi
